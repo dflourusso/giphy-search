71d152ee035e922feee119d58067dd58c112ac65
Daniel Fernando Lourusso <daniel.lourusso@db1.com.br>
2021-12-16 09:44:29 -0300

    build: configure styled-components

---
+++ screens/HomeScreen/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { Container, Title } from './styles'
+
+const HomeScreen: React.FC = () => {
+  return (
+    <Container>
+      <Title>Home Screen</Title>
+    </Container>
+  )
+}
+
+export default HomeScreen
