a666d0eaf9825f93e6213849d4fac13476084886
Daniel Fernando Lourusso <daniel.lourusso@db1.com.br>
2021-12-21 21:28:05 -0300

    feat: add service to search on giphy api

---
+++ src/constants/Env.ts
@@ -0,0 +1,6 @@
+const env = {
+  giphy_api_url: 'https://api.giphy.com/v1/gifs/search',
+  api_key: 'pLURtkhVrUXr3KG25Gy5IvzziV5OrZGa',
+}
+
+export default env
---
+++ src/services/gifs.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import env from '../constants/Env'
+
+const RECORDS_PERPAGE = 30
+
+export interface Gif {
+  id: string
+  url: string
+}
+
+type SearchParams = {
+  search: string
+  page?: number
+}
+
+const gifService = {
+  search: async ({ search, page = 1 }: SearchParams): Promise<Gif[]> => {
+    const offset = (page - 1) * RECORDS_PERPAGE
+
+    return axios
+      .get(env.giphy_api_url, {
+        params: {
+          q: search,
+          limit: RECORDS_PERPAGE,
+          offset,
+          api_key: env.api_key,
+        },
+      })
+      .then(({ data }) => data.data)
+  },
+}
+export default gifService
